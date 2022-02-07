dced247f051976823e63d28b77f25a456e3adf31
Minh Tan <MinhTan@Apples-MacBook-Pro.local>
2022-02-07 18:42:22 +0700

    Add substraction function

--- cal.py
+++ cal.py
@@ -19,3 +19,15 @@ def addition():
     return format_result(result)
 
 
+#Subtract two numbers
+@app.route('/subtract', methods=['POST','OPTIONS'])
+def substraction():
+    numbers = request.get_json(silent=True)
+
+    check = check_numbers(numbers)
+    if check == None:
+        result = {"result": numbers['n1'] - numbers['n2']}
+    else:
+        result = {"result": check}
+    
+    return format_result(result)
