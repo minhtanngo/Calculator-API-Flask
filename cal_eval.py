6a139f66407f47685cf0464f4f0ba108d20158b7
Minh Tan <MinhTan@Apples-MacBook-Pro.local>
2022-02-07 19:06:23 +0700

    Update function

--- cal_eval.py
+++ cal_eval.py
@@ -29,3 +29,29 @@ def cal():
         result = {"result" : check}
 
     return format_result(result)
+
+def check_expression(exp):
+
+    # Checks if expression contains any letters
+    if re.search("[a-zA-Z]", exp) != None:
+        return "Expression can't contain letters."
+
+    # Checks if expression ends with operator
+    if exp[-1] in "+-/*":
+        return "Expression is in the wrong format."
+    
+    return None
+
+def format_result(result):
+
+    # Turns result into a response object, with JSON data
+    result = json.jsonify(result)    
+    
+    # Add CORS headers to response
+    result.headers.add('Access-Control-Allow-Headers', 'content-type')
+    result.headers.add('Access-Control-Allow-Origin', '*')
+
+    return result
+
+if __name__ == '__main__':
+    app.run(debug=True)
